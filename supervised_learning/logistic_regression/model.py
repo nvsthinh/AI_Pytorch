4f47c5fa59c15fc9aa3442aeb558e0e1d0d10e74
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-07 15:46:44 +0700

    Init Logistic Regression model

---
+++ supervised_learning/logistic_regression/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self, n_inputs, n_outputs):
+        super().__init__()
+        self.linear = nn.Linear(n_inputs, n_outputs)
+    
+    def forward(self, x):
+        output = torch.sigmoid(self.linear(x))
+        return output
