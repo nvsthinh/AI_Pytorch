6815379829df137f3e841777bc80eef28a2f9963
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-09 13:52:55 +0700

    Update lenet.py

--- supervised_learning/deep_learning/cnn/lenet.py
+++ supervised_learning/deep_learning/cnn/lenet.py
@@ -2,20 +2,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LeNet(nn.Module):
-    def __init__(self):
+    def __init__(self, imdim=3, num_classes=10):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*5*5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+
+        self.conv1 = nn.Conv2d(imdim, 64, kernel_size=5, stride=3, padding=0)
+        self.mp = nn.MaxPool2d(2)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=3, padding=0)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.fc1 = nn.Linear(128*5*5, 1024)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.relu4 = nn.ReLU(inplace=True)
+        
+        self.fc3 = nn.Linear(1024, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        in_size = x.size(0)
+        out1 = self.mp(self.relu1(self.conv1(x)))
+        out2 = self.mp(self.relu2(self.conv2(out1)))
+        out2 = out2.view(in_size, -1)
+        out3 = self.relu3(self.fc1(out2))
+        out = self.relu4(self.fc2(out3))
+        
+        return self.fc3(out)
