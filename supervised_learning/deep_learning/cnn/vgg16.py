9658d471e7aad697b9ded50a9164cbfe16ec4512
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-09 22:17:10 +0700

    Update vgg16.py

--- supervised_learning/deep_learning/cnn/vgg16.py
+++ supervised_learning/deep_learning/cnn/vgg16.py
@@ -1,57 +1,65 @@
 import torch.nn as nn
+import torch
 
+""" VGG-16 Architecture """
 class VGG16(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=1000):
         super(VGG16, self).__init__()
-        self.relu = nn.ReLU(inplace=True)
         self.features = nn.Sequential(
+            # Conv Block 1
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
+            # Conv Block 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
+            # Conv Block 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
+            # Conv Block 4
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
+            # Conv Block 5
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            self.relu,
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         
         self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(512 * 1 * 1, 128),
-            self.relu,
-            nn.Linear(128, 128),
-            self.relu,
-            nn.Linear(128, 10)
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes)
         )
         
     def forward(self, x):
         x = self.features(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
