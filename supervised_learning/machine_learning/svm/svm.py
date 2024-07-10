93ecd113108fb78c6a698913fa32e2b26f7de2b9
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-10 23:10:31 +0700

    Update svm model

--- supervised_learning/machine_learning/svm/svm.py
+++ supervised_learning/machine_learning/svm/svm.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 
 class SVM(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim):
         super(SVM, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
+        self.linear = nn.Linear(input_dim, 1)
 
     def forward(self, x):
         return self.linear(x)
