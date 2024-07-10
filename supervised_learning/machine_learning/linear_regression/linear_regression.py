072cabc0911e3e7fdfcc20c6893ea83373c27dbf
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-10 15:20:02 +0700

    Add comment in linear_regression.py

--- supervised_learning/machine_learning/linear_regression/linear_regression.py
+++ supervised_learning/machine_learning/linear_regression/linear_regression.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class LinearRegressionModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear(1, 1)
+        self.linear = nn.Linear(1, 1) # 1 input and 1 output feature
     
     def forward(self, x):
         output = self.linear(x)
