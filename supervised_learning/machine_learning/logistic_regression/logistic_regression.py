e3309a7f58943ab3605e8771122b7e31c89ac3fd
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-11 11:42:40 +0700

    Add comment in logistic_regression.py

--- supervised_learning/machine_learning/logistic_regression/logistic_regression.py
+++ supervised_learning/machine_learning/logistic_regression/logistic_regression.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 class LogisticRegressionModel(nn.Module):
     def __init__(self, n_inputs, n_outputs):
         super().__init__()
-        self.linear = nn.Linear(n_inputs, n_outputs)
+        self.linear = nn.Linear(n_inputs, n_outputs)  # Define a linear layer with n_inputs and n_outputs
     
     def forward(self, x):
-        output = torch.sigmoid(self.linear(x))
+        output = torch.sigmoid(self.linear(x))  # Apply sigmoid activation to the linear transformation
         return output
