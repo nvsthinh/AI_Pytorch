c83b4238ee882424c5c66b89a34467bd7f94709d
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-11 09:30:15 +0700

    Update random forest to folder

--- supervised_learning/machine_learning/tree_based/__init__.py
+++ supervised_learning/machine_learning/tree_based/__init__.py
@@ -1,3 +1,3 @@
 from .binary_tree import hDecisionTreeClassifier, DecisionTreeRegressor
-from .random_forest import RandomForestClassifier, RandomForestRegressor
+from .random_forest.random_forest import RandomForestClassifier, RandomForestRegressor
 from .utils import sample_dimensions, divide_set, entropy, sample_vectors, split_function, unique_counts, log2, variance, mean
--- supervised_learning/machine_learning/tree_based/random_forest/random_forest.py
+++ supervised_learning/machine_learning/tree_based/random_forest/random_forest.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import torch
 
-from .binary_tree import DecisionTreeClassifier, DecisionTreeRegressor
-from .utils import sample_vectors, sample_dimensions
+from ..decision_tree.decision_tree import DecisionTreeClassifier, DecisionTreeRegressor
+from ..utils import sample_vectors, sample_dimensions
 
 
 class RandomForestClassifier(torch.nn.Module):
